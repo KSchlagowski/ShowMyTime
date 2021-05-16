3396cec096f67a15950926769958fcee0aafda92
KSchlagowski <mediewilnp@gmail.com>
2021-05-16 19:22:30 +0200

    Clear git cache

--- ShowMyTime.Domain/Models/ConfigurationModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ShowMyTime.Domain.Models
-{
-    public class ConfigurationModel
-    {
-        public string BaseApiPath { get; set; }
-        public string UserTimeZone { get; set; }
-        public string AllTimeZonesApiPath { get; set; }
-    }
-}
